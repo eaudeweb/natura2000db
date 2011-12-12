86518f9525d46b5d3dfdf754e07722b50b49616c
Alex Morega <alex@grep.ro>
2011-12-12 17:05:53 +0200

    proxy pages to backend database app

--- chmrio_zope2/chmrio_zope2/proxy.py
+++ chmrio_zope2/chmrio_zope2/proxy.py
@@ -1,8 +1,26 @@
+import urllib
 from Products.Five.browser import BrowserView
+from App.config import getConfiguration
+
+
+_env = getConfiguration().environment
+BACKEND_URL = _env.get('CHMRIO_BACKEND_URL').rstrip('/')
+
+
+def get_backend_page(name, **kwargs):
+    url = BACKEND_URL + name
+    if kwargs:
+        url += '?' + urllib.urlencode(kwargs.iteritems())
+
+    f = urllib.urlopen(url)
+    try:
+        return f.read()
+    finally:
+        f.close()
 
 
 class ChmRioFormsProxy(BrowserView):
 
     def __call__(self):
         # TODO view is not protected by ViewManagementScreens permission :(
-        return ":)\n"
+        return get_backend_page('/')
