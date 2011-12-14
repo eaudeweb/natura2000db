db9829598d0e9c317066088d2388d11b5651c626
Alex Morega <alex@grep.ro>
2011-12-14 16:58:03 +0200

    simplify form api

--- widgets.py
+++ widgets.py
@@ -6,25 +6,25 @@ _default_settings = {
     'skip_labels': False,
 }
 
-class MarkupGenerator(Generator):
-
-    def __init__(self):
-        super(MarkupGenerator, self).__init__('html')
-        self._frames[-1].update(_default_settings)
-
-
 class WidgetDispatcher(object):
 
     def __init__(self, jinja_env):
         self.jinja_env = jinja_env
 
-    def __call__(self, element, **kwargs):
+    def __call__(self, element):
         tmpl = self.jinja_env.get_template('widgets-edit.html')
         widget_name = element.properties.get('widget', 'input')
-        widget_impl = getattr(tmpl.module, widget_name)
-        return widget_impl(element, **kwargs)
+        widget_macro = getattr(tmpl.module, widget_name)
+        return widget_macro(element)
+
+
+class MarkupGenerator(Generator):
+
+    def __init__(self, jinja_env):
+        super(MarkupGenerator, self).__init__('html')
+        self._frames[-1].update(_default_settings)
+        self.widget = WidgetDispatcher(jinja_env)
 
 
 def install_widgets(jinja_env):
-    jinja_env.globals['form_generator'] = MarkupGenerator()
-    jinja_env.globals['widget'] = WidgetDispatcher(jinja_env)
+    jinja_env.globals['form'] = MarkupGenerator(jinja_env)
