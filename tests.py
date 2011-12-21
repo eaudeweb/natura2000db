63a38286170f3cf881e64c2c9d7ae7b35fcbe366
Alex Morega <alex@grep.ro>
2011-12-21 12:50:52 +0200

    test for adding a minimal document

--- tests.py
+++ tests.py
@@ -18,4 +18,24 @@ class FormsTest(unittest.TestCase):
         self.app = _create_test_app(self.tmp)
 
     def test_minimal_add(self):
-        pass
+        self.doc = None
+        def doc_saved(name, doc_id, doc):
+            self.doc_id = doc_id
+            self.doc = doc
+
+        self.app.document_signal.connect(doc_saved, sender='save')
+
+        client = self.app.test_client()
+        form_data = {
+            'section1_type': 'K',
+            'section1_sitecode': 'asdfqwer3',
+            'section1_date': '200503',
+            'section1_site_name': 'Firul Ierbii',
+            'section2_regcod_0_reg_code': '',
+            'section2_regcod_0_reg_name': 'Poiana',
+            'section2_regcod_0_cover': '',
+            'section2_bio_region_alpine': '1',
+        }
+        response = client.post('/new', data=form_data, follow_redirects=True)
+        self.assertIsNotNone(self.doc)
+        self.assertIn("Document %r saved" % self.doc_id, response.data)
