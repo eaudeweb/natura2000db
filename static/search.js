8318b420a52651ba6f7b4453b5dfb04aec2bbef6
Alex Morega <alex@grep.ro>
2011-12-26 19:57:01 +0200

    advanced search button; remember in cookie

--- static/search.js
+++ static/search.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
 
-    $('.search-advanced-button').click(function(evt) {
+    var advanced = 'search-advanced-visible';
+    var cookie_name = 'chm-rio-advanced-search';
+
+    $('.search-toggle').click(function(evt) {
         evt.preventDefault();
-        //$('.advanced-search').toggle();
-        $('.search-criteria').toggleClass('search-advanced-visible');
+        toggle_advanced_search();
     });
+
+    if($.cookie(cookie_name)) {
+        toggle_advanced_search();
+    }
+
+    function toggle_advanced_search() {
+        var on = $('.search-criteria').toggleClass(advanced).hasClass(advanced);
+        $.cookie(cookie_name, on ? 'on' : null, {expires: 1});
+    }
 
 });
