cace91a4a7cd33ff4cfd09bfa1f529080521d6e6
Alex Morega <alex@grep.ro>
2012-01-04 12:39:33 +0200

    "+" icon for the add-new-row button

--- static/form.js
+++ static/form.js
@@ -3,13 +3,12 @@ $(document).ready(function() {
 $('tr.table-append').each(function() {
     var tr = $(this);
     var tds = tr.children().remove();
-    var button = $('<a href="#">+</a>').click(function(evt) {
+    var button = $('<a href="#" class="add">').click(function(evt) {
         evt.preventDefault();
         var new_tr = $('<tr>').append(tds.clone());
         tr.before(new_tr);
     });
-    var td_button = $('<td>').attr('colspan', tds.length);
-    td_button.append('[', button, ']');
+    var td_button = $('<td>').attr('colspan', tds.length).append(button);
     tr.append(td_button).show();
 });
 
