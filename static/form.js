f8e61d3a8eb6c16fcb744708833573b555174ed8
Alex Morega <alex@grep.ro>
2012-01-05 12:17:35 +0200

    properly generate IDs for new list items

--- static/form.js
+++ static/form.js
@@ -4,12 +4,17 @@ $('.virtual-child').each(function() {
     var list = $(this).parent();
     var button = $('a.add', list);
     var item_with_button = $('> :has(a.add)', list);
-    var virtual = $(this).remove();
+    var virtual = $(this).remove().removeClass('virtual-child');
+    var html = $('<div>').append(virtual).html();
+    var next_id = Number(button.text());
+    button.text('');
 
     button.click(function(evt) {
         evt.preventDefault();
-        var new_item = virtual.clone().removeClass('virtual-child');
+        var next_html = html.replace(/NEW_LIST_ITEM/g, next_id);
+        var new_item = $('<div>').html(next_html).children();
         item_with_button.before(new_item);
+        next_id += 1;
     });
 });
 
