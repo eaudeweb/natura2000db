6a57ce8c3bbb9e0fdb22514c405ca8629d9b27fc
Alex Morega <alex@grep.ro>
2012-01-05 16:56:41 +0200

    statistics: allow filtering by nuts2

--- statistics.py
+++ statistics.py
@@ -4,16 +4,30 @@ import jinja2
 
 def area(search_form, search_answer):
     stat = {'table': [], 'total': 0}
+
+    if not search_form['nuts3'].is_empty:
+        search_nuts3 = search_form['nuts3'].value
+        def match_nuts3(code):
+            return (code == search_nuts3)
+
+    elif not search_form['nuts2'].is_empty:
+        search_nuts2 = search_form['nuts2'].value
+        def match_nuts3(code):
+            return code.startswith(search_nuts2)
+
+    else:
+        raise ValueError("Either a nuts3 or nuts2 code must be specified")
+
     for doc in search_answer['docs']:
         data = doc['data']
         total_area = data['section2']['area']
 
-        admin_code = search_form['nuts3'].value
         for admin in data['section2']['administrative']:
-            if admin['code'] == admin_code:
+            if match_nuts3(admin['code']):
                 break
         else:
-            raise ValueError('no matching nuts3 %r' % admin_code)
+            raise ValueError('no matching nuts3, doc_id=%r'
+                             % data['section1']['code'])
 
         admin_area = total_area * (admin['coverage'] / 100)
         stat['total'] += admin_area
