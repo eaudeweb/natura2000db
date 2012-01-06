c8d824c363dbc0798e400da8a5930b9c5eca11f7
Alex Morega <alex@grep.ro>
2012-01-06 12:31:51 +0200

    show map in search results

--- static/search.js
+++ static/search.js
@@ -29,4 +29,17 @@ $(document).ready(function() {
         }
     });
 
+    $('#search-results-map').each(function() {
+
+        var map = new L.Map(this, {
+            center: new L.LatLng(46, 25.0),
+            zoom: 6
+        });
+
+        var osm_url = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        var osm = new L.TileLayer(osm_url, {maxZoom: 18});
+        map.addLayer(osm);
+
+    });
+
 });
