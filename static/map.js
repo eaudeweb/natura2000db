794ef6ec4e54d6416d849649cd1c37d36c8eba6b
Alex Morega <alex@grep.ro>
2012-01-06 17:19:23 +0200

    a bit of styling for map polygons

--- static/map.js
+++ static/map.js
@@ -4,15 +4,22 @@ $(document).ready(function() {
 
         var map = new L.Map(this, {
             center: new L.LatLng(46, 25.0),
-            zoom: 6
+            zoom: 6,
+            fadeAnimation: false
         });
 
         var osm_url = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         var osm = new L.TileLayer(osm_url, {maxZoom: 18});
         map.addLayer(osm);
 
-        var shapes = new L.GeoJSON();
-        map.addLayer(shapes);
+        var layers = {
+            sci: new L.GeoJSON(),
+            spa: new L.GeoJSON()
+        };
+        R.layers = layers;
+
+        map.addLayer(layers['sci']);
+        map.addLayer(layers['spa']);
 
         var sitecode_hash = {};
         $('.search-results .sitecode').each(function() {
@@ -23,12 +30,20 @@ $(document).ready(function() {
 
         $.getJSON(R.assets + 'sci-wgs84.geojson', function(data) {
             data['features'] = filter_features(data['features'], keep);
-            shapes.addGeoJSON(data);
+            layers['sci'].addGeoJSON(data);
+            layers['sci'].setStyle({
+                color: '#b92',
+                weight: 2
+            });
         });
 
         $.getJSON(R.assets + 'spa-wgs84.geojson', function(data) {
             data['features'] = filter_features(data['features'], keep);
-            shapes.addGeoJSON(data);
+            layers['spa'].addGeoJSON(data);
+            layers['spa'].setStyle({
+                color: '#9b2',
+                weight: 2
+            });
         });
 
         function filter_features(features, keep) {
