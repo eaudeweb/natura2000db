32e7455e77ef7b2a9443879a453ae6dcd92a5a9d
Alex Morega <alex@grep.ro>
2012-01-10 12:33:42 +0200

    change h1 of new/edit pages

--- webpages.py
+++ webpages.py
@@ -30,7 +30,12 @@ def view():
     doc = db.load_document(doc_id)
     form = widgets.MarkupGenerator(flask.current_app.jinja_env)
     form.other_site_labels = _other_site_labels(doc)
-    return flask.render_template('view.html', form=form, doc=doc, doc_id=doc_id)
+    return flask.render_template('view.html', title=_doc_title(doc), form=form,
+                                 doc=doc, doc_id=doc_id)
+
+
+def _doc_title(doc):
+    return u"%s (%s)" % (doc['section1']['name'].u, doc['section1']['code'].u)
 
 
 @webpages.route('/new', methods=['GET', 'POST'])
@@ -38,6 +43,7 @@ def view():
 def edit():
     doc_id = flask.request.args.get('doc_id', None)
     db = get_db()
+    new_doc = bool(doc_id is None)
 
     if flask.request.method == 'POST':
         doc = schema.SpaDoc.from_flat(flask.request.form.to_dict())
@@ -53,13 +59,15 @@ def edit():
             flask.flash("Errors in document")
 
     else:
-        if doc_id is None:
+        if new_doc:
             doc = schema.SpaDoc()
         else:
             doc = db.load_document(doc_id)
 
+    title = u"Introducere sit nou" if new_doc else _doc_title(doc)
     form = widgets.MarkupGenerator(flask.current_app.jinja_env)
-    return flask.render_template('edit.html', doc=doc, form=form)
+    return flask.render_template('edit.html', title=title,
+                                 doc=doc, form=form)
 
 
 def _db_search(search_form):
