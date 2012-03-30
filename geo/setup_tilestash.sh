ec5c853a404277a47c68a5eeb58b57d1a29362c7
Alex Morega <alex@grep.ro>
2012-03-30 18:22:20 +0300

    render production tiles

--- geo/setup_tilestash.sh
+++ geo/setup_tilestash.sh
@@ -7,3 +7,4 @@ pip install tilestache werkzeug modestmaps
 deactivate
 #cd /natura2000db/geo/
 #~/mapping/bin/tilestache-server.py -i 0.0.0.0
+#~/mapping/bin/tilestache-seed.py -c tilestache-disk.cfg -b 44 20 49 31 -l all-sites 1 2 3 4 5 6 7 8 9 10 11 12
