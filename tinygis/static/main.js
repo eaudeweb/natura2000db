bd166e084cf737920ca0058d7fb012966caf2fa6
Alex Morega <alex@grep.ro>
2012-04-06 18:13:29 +0300

    wrap main in a function

--- tinygis/static/main.js
+++ tinygis/static/main.js
@@ -1,45 +1,49 @@
-$(function () {
+(function() {
 
-TG.load_templates();
+TG.main = function() {
+    TG.load_templates();
 
-TG.map = new TG.Map({parent: $('body')[0]});
-TG.map.addLayer(new TG.TileLayer({
-    name: "SCI + SPA",
-    url_template: '/static/tiles/all-sites/${z}/${x}/${y}.png'
-}));
-if(window.google && window.google.maps) {
-    TG.map.addGoogleLayers();
-}
-if(TG['BING_MAPS_KEY']) {
-    TG.map.addBingLayers(TG['BING_MAPS_KEY']);
-}
-
-TG.FeatureCollection.prototype.urlRoot = '/map/userlayers';
-
-$.get('/map/userlayers').done(function(data) {
-    var id_list = data['ids'];
-    var layer_id = null;
-    if(id_list.length > 0) {
-        layer_id = id_list[0];
+    TG.map = new TG.Map({parent: $('body')[0]});
+    TG.map.addLayer(new TG.TileLayer({
+        name: "SCI + SPA",
+        url_template: '/static/tiles/all-sites/${z}/${x}/${y}.png'
+    }));
+    if(window.google && window.google.maps) {
+        TG.map.addGoogleLayers();
+    }
+    if(TG['BING_MAPS_KEY']) {
+        TG.map.addBingLayers(TG['BING_MAPS_KEY']);
     }
 
-    TG.featureCollection = new TG.FeatureCollection({id: layer_id});
-    if(layer_id !== null) {
-        TG.featureCollection.fetch();
-    }
-    TG.featureCollectionEditor = new TG.FeatureCollectionEditor({
-        model: TG.featureCollection});
-    TG.featureCollectionEditor.$el.appendTo($('body'));
+    TG.FeatureCollection.prototype.urlRoot = '/map/userlayers';
 
-    TG.vectorLayer = new TG.VectorLayer({
-        model: TG.featureCollection,
-        proj: _.bind(TG.map.project, TG.map)
+    $.get('/map/userlayers').done(function(data) {
+        var id_list = data['ids'];
+        var layer_id = null;
+        if(id_list.length > 0) {
+            layer_id = id_list[0];
+        }
+
+        TG.featureCollection = new TG.FeatureCollection({id: layer_id});
+        if(layer_id !== null) {
+            TG.featureCollection.fetch();
+        }
+        TG.featureCollectionEditor = new TG.FeatureCollectionEditor({
+            model: TG.featureCollection});
+        TG.featureCollectionEditor.$el.appendTo($('body'));
+
+        TG.vectorLayer = new TG.VectorLayer({
+            model: TG.featureCollection,
+            proj: _.bind(TG.map.project, TG.map)
+        });
+        TG.map.olMap.addLayer(TG.vectorLayer.olLayer);
     });
-    TG.map.olMap.addLayer(TG.vectorLayer.olLayer);
-});
 
-var sidebar = new TG.Sidebar();
-sidebar.on('resize', TG.map.updateSize, TG.map);
-var mapLayers = new TG.MapLayers({"collection": TG.map.baseLayerCollection});
+    var sidebar = new TG.Sidebar();
+    sidebar.on('resize', TG.map.updateSize, TG.map);
+    var mapLayers = new TG.MapLayers({"collection": TG.map.baseLayerCollection});
+};
 
-});
+$(document).ready(TG.main);
+
+})();
