149b73d21dfb8505a10b339186722266fe8a84c5
Dragos Catarahia <dragos.catarahia@gmail.com>
2012-04-06 17:14:29 +0300

    main.js

--- tinygis/static/sidebar.js
+++ tinygis/static/sidebar.js
@@ -1,61 +1,56 @@
 (function () {
 
-    var Sidebar = Backbone.View.extend({
+TG.Sidebar = Backbone.View.extend({
 
-        id: "sidebar-container",
+    id: "sidebar-container",
 
-        events: {
-            "click #togglebar": "togglebar"
-        },
+    events: {
+        "click #togglebar": "togglebar"
+    },
 
-        initialize: function () {
-            this.render();
-        },
+    initialize: function () {
+        this.render();
+    },
 
-        render: function () {
-            /* <div id="sidebar-container">
-                   <div id="togglebar"></div>
-                   <div id="sidebar"></div>
-               </div>
-            */
+    render: function () {
+        /* <div id="sidebar-container">
+               <div id="togglebar"></div>
+               <div id="sidebar"></div>
+           </div>
+        */
 
-            var togglebar = this.make("div", {"id": "togglebar"});
-            var sidebar = this.make("div", {"id": "sidebar"});
+        var togglebar = this.make("div", {"id": "togglebar"});
+        var sidebar = this.make("div", {"id": "sidebar"});
 
-            $("body").append(
-                this.$el
-                    .append(togglebar)
-                    .append(sidebar)
-            );
-        },
+        $("body").append(
+            this.$el
+                .append(togglebar)
+                .append(sidebar)
+        );
+    },
 
-        togglebar: function () {
-            var sidebar = $("#sidebar");
-            var self = this;
-            if(sidebar.width() === 0) {
-                sidebar.animate({"width": 180, "padding": 20}, 250);
-                 $("#togglebar").animate({"left": 220}, 250);
-                 $("#tinygis-map").animate({"left": 220}, 250, function () {
-                    self.trigger("resize");
-                 });
-            } else {
-                 sidebar.animate({"width": 0, "padding": 0}, 250);
-                 $("#togglebar").animate({"left": 0}, 250);
-                 $("#tinygis-map").animate({"left": 0}, 250, function () {
-                    self.trigger("resize");
-                 });
-            }
-        },
-
-        attachView: function () {
-
+    togglebar: function () {
+        var sidebar = $("#sidebar");
+        var self = this;
+        if(sidebar.width() === 0) {
+            sidebar.animate({"width": 180, "padding": 20}, 250);
+             $("#togglebar").animate({"left": 220}, 250);
+             $("#tinygis-map").animate({"left": 220}, 250, function () {
+                self.trigger("resize");
+             });
+        } else {
+             sidebar.animate({"width": 0, "padding": 0}, 250);
+             $("#togglebar").animate({"left": 0}, 250);
+             $("#tinygis-map").animate({"left": 0}, 250, function () {
+                self.trigger("resize");
+             });
         }
+    },
 
-    });
+    attachView: function () {
 
-    $(function () {
-        var sidebar = new Sidebar();
-        sidebar.attachView();
-    });
+    }
+
+});
 
 })();
