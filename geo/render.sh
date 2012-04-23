2fff5d1d73a4677a8c453ed90da12d045529104e
Alex Morega <alex@grep.ro>
2012-04-23 11:59:06 +0300

    extend tiles bbox

--- geo/render.sh
+++ geo/render.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
 CMD="/home/vagrant/mapping/bin/tilestache-seed.py -c tilestache-disk.cfg"
-BBOX="-b 44 20 49 31"
+BBOX="-b 43 19 49 31"
 ZOOMS="1 2 3 4 5 6 7 8 9 10 11 12 13"
+
+#$CMD $BBOX -l $@ $ZOOMS
+
 $CMD $BBOX -l conservare-scispa $ZOOMS
 $CMD $BBOX -l conservare-etc $ZOOMS
 $CMD $BBOX -l administrativ $ZOOMS
